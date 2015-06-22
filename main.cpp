50d0cd8a2497eff83d138fdfe3ec5a1014000162
Yaroslav <C:\Users\mitas\AppData\Roaming\The Bat!>
2015-06-23 02:51:22 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,111 +1,80 @@
 #include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <string>
-#include <ctime>
 using namespace std;
- 
 
-const char* f(const char* pass){
-	
-	char alf[]="1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 	// Альфавит
-    //char pass[]="3241";									// Час пароль
+// Параметр функции-ссылка со значением по умолчанию
 
-    int size = sizeof(alf) / sizeof(char); 
-    char* pass_serch = new char[7];
-    //cout << "size:" << size << endl;
-    //unsigned int n = 0;
-    for(int i=0;i<=size;i++){
-        for(int j=0;j<=size;j++){
-            for(int k=0;k<=size;k++){
-                for(int l=0;l<=size;l++){
-                	for(int g=0;g<=size;g++){
-                		for(int h=0;h<=size;h++){    
-                		//n++;
-                   		pass_serch[0]=alf[i];
-                    	pass_serch[1]=alf[j];
-                    	pass_serch[2]=alf[k];
-                    	pass_serch[3]=alf[l];
-                    	pass_serch[4]=alf[g];
-                    	pass_serch[5]=alf[h];
-                    	pass_serch[6]='\0';
-                   
-                    	if(strcmp(pass_serch, pass)==0){
-                       		//cout << "n:" << n << endl;
-                       		return pass_serch;
-                    		}
-                    	}
-                    }
-                }
-            }
-        }
-    }
-    //cout << "n:" << n << endl;
-    return "Error";
+// 1
+int foo1(int a, int b = 5);
+
+// 2
+int foo2(int a, const int &b = 111);
+
+// 3
+static int d = 222;
+int foo3(int a, int &b = d);
+
+
+
+int main(int argc, char *argv[]){
+	// 1
+	cout<< "foo1:" << foo1(1, 2) << endl;
+	cout<< "foo1:" << foo1(1) << endl;
+	// 2
+	int a = 10;
+	cout<< "foo2:" << foo2(1, a) << endl;
+	cout<< "a:" << a << endl;
+	cout<< "foo2:" << foo2(1) << endl;
+	// 3
+	int aa = 55;
+	cout<< "foo3:" << foo3(1, aa) << endl;
+	cout<< "aa:" << aa << endl;
+	cout<< "foo3:" << foo3(1) << endl; 	// Дает возможность параметка ссылки со значением по умолчанию.
+
+	cout<<"========================="<<endl;
+
+	// Логические операции (ИЛИ)
+	if(0 || 0){
+		cout<<"true"<<endl;
+	}else{
+		cout<<"false"<<endl;
+	}
+
+	// Логические операции (И)
+	if(0 && 1){
+		cout<<"true"<<endl;
+	}else{
+		cout<<"false"<<endl;
+	}
+
+	cout<<"========================="<<endl;
+
+	// Логика While (0-9), работает пока true
+	int i = 0;
+	while(i < 10){
+		cout << "i:"<< i << endl;
+		i++;
+	}
+
+	cout<<"========================="<<endl;
+
+	// Тип Typedef в for()
+
+	for(size_t i = 0; i < 10; i++)
+		cout<<"i:"<< i << endl;
+	return 0;
 }
 
-
-//char alf[]="1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 	// Альфавит
-char alf[]="abcdefghijklmnopqrstuvwxyz"; 	// Альфавит
-//char alf[]="123"; 	// Альфавит
-unsigned int sizeAlf = sizeof(alf) / sizeof(char); // количество символо в алфавите.
-int k = -1;	                                //
-const int n = 5; 							// количество символов в пароле
-char a[n];              					// Ответиы
-char pass[] = "zzzzzz";						// пароль
-int sizePas = sizeof(pass) / sizeof(char); 	// количество символо в пароле. 
-unsigned int v = 0;
-
-void per(int &k){
-	
-	// 1. условия выхода
-	if(k >= (sizePas - 1)){		
-		//return;
-	}else
-	
-	// 2. условия выхода
-	if(strcmp(a, pass) == 0){
-    	cout << "Пароль найден:" << a << endl;
-    	sizeAlf = 0;
-   	}else{
-   		k++;
-		unsigned int i = 0;
-		for(; i < sizeAlf; i++){
-			a[k] = alf[i];
-			per(k);
-		}		
-		k--; 	                   
-	}	
+int foo1(int a, int b){
+	return a+b;
 }
 
+// Константа (недает изменять)
+int foo2(int a, const int &b){
 
-int main(){
- 	setlocale(0, "rus");
-
- 	unsigned int startTime = clock();
- 	cout << "time:" << startTime  << endl;
-
- 	//cout << "out:" << f("mita") << endl;	
-
-    per(k);
-
-    cout << "time:" << clock() << endl;
-	cout << "time:" << (clock() - startTime) << " mil/sec" << endl;
-   	 
-    return 0;
+	return a + b;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int foo3(int a, int &b){
+	b = b + 4;
+	return a + b;
+}
